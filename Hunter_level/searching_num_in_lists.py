c38299e6fc6627e8863d8a33b5b9d4fdeaa00cba
Sangeethraj 100 <sangeethraj3498@gmail.com>
2018-03-13 15:52:11 +0530

    Create searching_num_in_lists.py

---
+++ Hunter_level/searching_num_in_lists.py
@@ -0,0 +1,10 @@
+a=["0","1","2","3","4","5","6","7","8","9"]
+b=["0","1","2","3","4","5","6","7","8","9"]
+n=[]
+no=input("Enter two numbres to be checked in array:")
+n.append(no)
+print (no)
+if ((no in a) and (no in b)):
+    print "Yes"
+else:
+    print "No"
